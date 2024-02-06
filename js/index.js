ffeb5bd876eccc53bdacac7614f6eae7dcf5ad2f
carlosf90 <josecarlosgofilho@hotmail.com>
2024-02-06 12:04:58 -0300

    começo do pagode

---
+++ js/index.js
@@ -0,0 +1,15 @@
+mes.onchange = () => {
+    switch(mes.value.toLowerCase()){
+    case "janeiro"
+    nome.innerHTML = "Rei":
+    break;
+
+    case"Fevereiro":
+    nome.innerHTML = "Mô"
+
+
+
+
+    }
+    
+}
